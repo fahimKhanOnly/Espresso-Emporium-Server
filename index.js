94e9863952ad692d88e718f8af1b92287e7d168d
fahimKhanOnly <fahimmontacir767@gmail.com>
2024-12-07 21:31:37 +0600

    Change something.

--- index.js
+++ index.js
@@ -88,7 +88,6 @@ async function run() {
       res.send(result);
     })
 
-
     app.get("/users", async (req, res) => {
       const cursor = usersDB.find();
       const result = await cursor.toArray();
